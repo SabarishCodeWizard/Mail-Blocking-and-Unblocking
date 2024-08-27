c570372e84f091f9f6af92734cdf2a8a52506097
SabarishCodeWizard <ravik60656@gmail.com>
2024-08-27 14:20:49 +0530

    google firebase authentication npm integrated success

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import axios from 'axios';
 import './Login.css'; // Import the CSS file
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCGxYuVK8K5CYQAhbYbHf9VCkSfCdvEQSw",
+    authDomain: "mail-dc436.firebaseapp.com",
+    projectId: "mail-dc436",
+    storageBucket: "mail-dc436.appspot.com",
+    messagingSenderId: "34622970017",
+    appId: "1:34622970017:web:a92267616e006ae63078e0",
+    measurementId: "G-JYG2W6RKBN"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 const Login = ({ onLoginSuccess }) => {
     const [username, setUsername] = useState('');
@@ -16,6 +33,17 @@ const Login = ({ onLoginSuccess }) => {
             onLoginSuccess();
         } catch (err) {
             setError('Invalid credentials');
+        }
+    };
+
+    const handleGoogleLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, provider);
+            const token = await result.user.getIdToken();
+            localStorage.setItem('authToken', token);
+            onLoginSuccess();
+        } catch (err) {
+            setError('Google authentication failed');
         }
     };
 
@@ -39,7 +67,12 @@ const Login = ({ onLoginSuccess }) => {
                 />
                 <button type="submit">Login</button>
             </form>
-            {error && <p className="error">{error}</p>}
+            <div className='google-container'>
+                <button onClick={handleGoogleLogin} className="google">
+                    Sign in with Google
+                </button>
+                {error && <p className="error">{error}</p>}
+            </div>
         </div>
     );
 };
